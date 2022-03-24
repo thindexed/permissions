5c87c26c5438cf9f6fd710800fba5a0d3626c46f
Herz, Andreas <andreas.herz@sap.com>
2022-03-24 22:47:00 +0100

    no message

---
+++ src/python/main.py
@@ -0,0 +1,26 @@
+import logging
+import json
+import pathlib
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
+
+print(__package__)
+data_anonym = json.loads(pathlib.Path(__file__).parent.joinpath('permissions-anonym.json').read_text())
+data_user = json.loads(pathlib.Path(__file__).parent.joinpath('permissions-user.json').read_text())
+
+@app.route('/permissions')
+def permissions():
+    role = request.headers.get('your-header-name')        
+    if role == "user":
+        return jsonify(data_user)
+
+    return jsonify(data_anonym)
+
+if __name__ == '__main__':
+    print("AuthZ Server Up and running")
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=False)
+
+
