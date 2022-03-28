5a3e4b40a24b518c5c8f43942bfc7638bcb72afd
Herz, Andreas <andreas.herz@sap.com>
2022-03-28 10:38:52 +0200

    no message

--- src/python/main.py
+++ src/python/main.py
@@ -9,12 +9,16 @@ app.logger.setLevel(logging.DEBUG)
 
 print(__package__)
 data_anonym = json.loads(pathlib.Path(__file__).parent.joinpath('permissions-anonym.json').read_text())
+data_admin = json.loads(pathlib.Path(__file__).parent.joinpath('permissions-admin.json').read_text())
 data_user = json.loads(pathlib.Path(__file__).parent.joinpath('permissions-user.json').read_text())
 
 @app.route('/permissions')
 def permissions():
     role = request.headers.get('x-role')   
     print("role:", role)     
+    if role == "admin":
+        return jsonify(data_admin)
+
     if role == "user":
         return jsonify(data_user)
 
